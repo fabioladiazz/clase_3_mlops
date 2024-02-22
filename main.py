88028a64c5ac51c23a1a025b406a211c874195e0
Fabiola Diaz Garita <fabidg142517@gmail.com>
2024-02-22 16:45:15 -0600

    funcion resta

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def suma(num1: int, num2: int) -> int:
     return num1 + num2
 
+
+def resta(num1: int, num2: int) -> int:
+    return num1 - num2
+
+
 if __name__ == '__main__':
     print(suma(num1=2, num2=4))
+    print(resta(num1=2, num2=4))
